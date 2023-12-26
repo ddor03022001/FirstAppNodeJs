b441beac4773946d994b35b348276d14eef6d17a
ddor03022001 <huyqrtqn@gmail.com>
2023-12-26 11:13:39 +0700

    first commit

---
+++ src/seeders/seeder_user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Huy',
+      lastName: 'Tran',
+      address: 'VietNam',
+      gender: 1,
+      roleId: 'R1',
+      phoneNumber: '03456789',
+      positionId: 'doctor',
+      image: 'hello',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
---
+++ src/services/CRUDService.js
@@ -0,0 +1,111 @@
+import bcrypt from 'bcryptjs';
+import db from '../models/index';
+
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+            })
+
+            resolve('succesfull')
+
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword);
+        } catch (e) {
+            reject(e);
+        }
+
+    })
+}
+let getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = db.User.findAll({
+                raw: true,
+            });
+            resolve(users)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+let getUserInfoById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                raw: true,
+            })
+
+            if (user) {
+                resolve(user)
+            }
+            else {
+                resolve([])
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+
+                await user.save();
+
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            } else {
+                resolve();
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+let deleteUserById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId }
+            })
+
+            if (user) {
+                await user.destroy();
+            }
+
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+module.exports = {
+    createNewUser: createNewUser,
+    getAllUser: getAllUser,
+    getUserInfoById: getUserInfoById,
+    updateUserData: updateUserData,
+    deleteUserById: deleteUserById,
+}
