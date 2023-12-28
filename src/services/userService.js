0d72b422f0f7a359994cc2ec51a80457839adada
ddor03022001 <huyqrtqn@gmail.com>
2023-12-28 16:59:10 +0700

    commit

--- src/services/userService.js
+++ src/services/userService.js
@@ -122,7 +122,7 @@ let createNewUser = (data) => {
                     gender: data.gender,
                     roleId: data.roleId,
                     positionId: data.positionId,
-
+                    image: data.avatar,
                 })
 
                 resolve({
@@ -160,7 +160,7 @@ let deleteUser = (userId) => {
 let updateReactUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'Missing required parameters!'
@@ -174,6 +174,13 @@ let updateReactUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phoneNumber = data.phoneNumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
 
                 await user.save()
                 resolve({
